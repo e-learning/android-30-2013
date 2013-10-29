979ebdd1308ab860634e5ee8a13a63dc9c2d37f4
LDVSOFT <lapshin.dv@mail.ru>
2013-10-29 18:32:42 +0400

    Button listener

--- lapshin.dmitry/DreamCalculator/src/ru/spb/school30/ldvsoft/calc/MainActivity.java
+++ lapshin.dmitry/DreamCalculator/src/ru/spb/school30/ldvsoft/calc/MainActivity.java
@@ -2,6 +2,9 @@ package ru.spb.school30.ldvsoft.calc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +14,15 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		final Button btn = (Button) findViewById(R.id.buttonCalculate);
+		btn.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				Toast.makeText(MainActivity.this, "Hello! Is it me you're looking for?", Toast.LENGTH_LONG).show();
+			}
+		});
     }
 }
