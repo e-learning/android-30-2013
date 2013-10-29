82abf5f1e71b45366c0b3a2783f1b931deffbe03
mininMD <mininmd96@gmail.com>
2013-10-29 18:47:04 +0400

    Add layout of calculator

---
+++ untitled/gen/com/gmail/mininmd96/calculator/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.gmail.mininmd96.calculator;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
