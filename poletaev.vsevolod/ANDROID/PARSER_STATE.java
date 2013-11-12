823de86f8640fe257ef9233e91edd92ed4238f1a
Vitaly A. Galinsky <cgsg@yandex.ru>
2013-11-12 16:31:27 +0400

    delete

--- poletaev.vsevolod/ANDROID/PARSER_STATE.java
+++
@@ -1,6 +0,0 @@
-package com.example.Calculator;
-
-// Parser state enumeration type
-public enum PARSER_STATE {
-    STATE_WAIT_PREFIX, STATE_WAIT_SUFFIX, STATE_ERROR, STATE_DONE, STATE_END
-}
--- poletaev.vsevolod/ANDROID/TOKEN_ID.java
+++
@@ -1,6 +0,0 @@
-package com.example.Calculator;
-
-// Tokens id enumeration type
-public enum TOKEN_ID {
-    TOKEN_OPERAND, TOKEN_OPERATOR
-}
