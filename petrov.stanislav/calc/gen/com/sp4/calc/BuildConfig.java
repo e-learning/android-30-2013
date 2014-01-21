cbf93a238377b1fb4a6470d4bc44be7674c020a1
unknown <SEMI14@R111-31.(none)>
2014-01-21 19:13:52 +0400

    new version

---
+++ petrov.stanislav/calc/gen/com/sp4/calc/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.sp4.calc;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ petrov.stanislav/calc/src/com/sp4/calc/About.java
@@ -0,0 +1,21 @@
+package com.sp4.calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: SEMI14
+ * Date: 17.12.13
+ * Time: 20:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class About extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
